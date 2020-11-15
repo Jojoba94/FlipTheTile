7e76bab0473546958d9435acb6a9c5750aae65e3
Eu Jeen <EuJeen_94@hotmail.com>
2020-11-15 23:28:07 +0800

    add fonts and MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _ScreenShade;
+    [SerializeField] private AnimationCurve curve;
+
+    private Image _image;
+
+    private void Start()
+    {
+        _ScreenShade.SetActive(false);
+        _image = _ScreenShade.GetComponent<Image>();
+    }
+
+    public void Play()
+    {
+        _ScreenShade.SetActive(true);
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float t = 1f;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, a);
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            _image.color = new Color(_image.color.r, _image.color.g, _image.color.b, a);
+            yield return 0;
+        }
+    }
+
+}
