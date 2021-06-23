4dc965c85bf348d2f1f2c6fd46d4d12d9a131633
Eu Jeen <EuJeen_94@hotmail.com>
2021-06-23 23:15:42 +0800

    Add Player UI and Match Manager

---
+++ Assets/Scripts/MatchManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    private int _timePeriodSeconds = 60;
+
+    private async void Start()
+    {
+        await CountDown();
+    }
+
+    private async Task CountDown()
+    {
+        for (int i = 0; i < _timePeriodSeconds; i++)
+        {
+            await Task.Delay(1000);
+            //Debug.Log(Time.time);
+        }
+    }
+
+
+}
